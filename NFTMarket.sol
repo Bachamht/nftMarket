b65d0de7ea52af15943aeebcc7b35cc666d7a141
agito <1634263395@qq.com>
2024-01-16 14:38:14 +0800

    continue to improve

--- NFTMarket.sol
+++ NFTMarket.sol
@@ -2,17 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-/*
-• 发行一个 ERC721 Token(用自己的名字)
-
-• 铸造几个 NFT,在测试网上发行,在 Opensea 上查看
-
-• 编写一个市场合约:使用自己发行的ERC20 Token 来买卖NFT:
-
-• NFT 持有者可上架 NFT(list 设置价格 多少个 TOKEN 购买 NFT )
-
-• 编写购买NFT 方法 buyNFT(uint tokenID, uint amount),转入对应的TOKEN,获取对应的 NFT
-*/
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -20,8 +9,10 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 interface IERC721 {
         function safeTransferFrom(address from, address to, uint256 tokenId) external;
         function ownerOf(uint256 tokenId) external returns(address);
+        function approve(address to, uint256 tokenId) external;
 
 }
+
 contract NFTMarket {
 
     using SafeERC20 for IERC20;
@@ -75,13 +66,18 @@ contract NFTMarket {
     //用户买nft
     function buyNFT(uint256 tokenID, uint256 amount) public isOnSelling(tokenID) NotEnough(amount, tokenID) {
        
-        address sellerOwner = IERC721(nftAddr).ownerOf(tokenID);
-        IERC20(tokenPool).safeTransferFrom(msg.sender, sellerOwner, amount);
-        address preOwner = IERC721(nftAddr).ownerOf(tokenID);
+        address seller = IERC721(nftAddr).ownerOf(tokenID);
+        IERC20(tokenPool).approve(seller, amount);
+        IERC20(tokenPool).safeTransferFrom(msg.sender, seller, amount);
         IERC721(nftAddr).approve(msg.sender, tokenID);
-        IERC721(nftAddr).safeTransferFrom(preOwner, msg.sender, tokenID) ;
+        IERC721(nftAddr).safeTransferFrom(seller, msg.sender, tokenID) ;
         emit BuySuccess(msg.sender, tokenID);
 
+    }
+
+    //用户查询nft的主人
+    function viewOwner(uint256 tokenID) public returns(address) {
+        return IERC721(nftAddr).ownerOf(tokenID);
     }
 
 
---
+++ README.md
@@ -0,0 +1,16 @@
+## 功能测试
+
+NFT部署后opensea上查看：
+
+![image]()
+
+
+
+在NFT market 上架出售：
+
+![image]()
+
+
+
+此时查看上架的NFT的主人：
+![image]()
